0a4be9e948220893f1052eb1b2362a94f0fb7ec2
dianamartine <diana_martine@outlook.com>
2021-10-26 17:47:45 -0300

    creating a relational data access with jdbc and h2

---
+++ src/test/java/com/example/relationaldataaccess/RelationalDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationaldataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
